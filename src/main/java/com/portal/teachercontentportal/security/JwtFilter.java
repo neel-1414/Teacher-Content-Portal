8a9f85f7007fc5c4607ca5e8e3ce0c8750104efb
Harshit Panchariya <harshitcourses1@gmail.com>
2026-03-11 22:34:37 +0530

    Added JWT tokens code

--- src/main/java/com/portal/teachercontentportal/security/JwtFilter.java
+++ src/main/java/com/portal/teachercontentportal/security/JwtFilter.java
@@ -1,4 +1,32 @@
 package com.portal.teachercontentportal.security;
 
-public class JwtFilter {
+import com.auth0.jwt.interfaces.DecodedJWT;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if(header != null && header.startsWith("Bearer ")) {
+
+            String token = header.substring(7);
+
+            DecodedJWT jwt = JwtUtil.validateToken(token);
+
+            String teacherId = jwt.getSubject();
+
+            System.out.println("Authenticated user: " + teacherId);
+        }
+
+        chain.doFilter(request, response);
+    }
 }
--- src/main/java/com/portal/teachercontentportal/security/JwtUtil.java
+++ src/main/java/com/portal/teachercontentportal/security/JwtUtil.java
@@ -1,4 +1,32 @@
 package com.portal.teachercontentportal.security;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Date;
 
 public class JwtUtil {
+
+    private static final String SECRET = "mysecretkey";
+
+    public static String generateToken(String teacherId, String role) {
+
+        Algorithm algorithm = Algorithm.HMAC256(SECRET);
+
+        return JWT.create()
+                .withSubject(teacherId)
+                .withClaim("role", role)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 3600000))
+                .sign(algorithm);
+    }
+    public static DecodedJWT validateToken(String token) {
+
+        Algorithm algorithm = Algorithm.HMAC256(SECRET);
+
+        return JWT.require(algorithm)
+                .build()
+                .verify(token);
+    }
+
 }
