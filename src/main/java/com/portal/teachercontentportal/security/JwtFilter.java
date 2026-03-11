f958061df064c9e6a0f83bd2020bfde4757cf47c
neel-1414 <neelyshinde1@gmail.com>
2026-03-11 22:56:11 +0530

    minor changes

--- src/main/java/com/portal/teachercontentportal/security/JwtFilter.java
+++ src/main/java/com/portal/teachercontentportal/security/JwtFilter.java
@@ -9,21 +9,13 @@ import java.io.IOException;
 
 public class JwtFilter extends OncePerRequestFilter {
 
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain chain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
 
         String header = request.getHeader("Authorization");
-
         if(header != null && header.startsWith("Bearer ")) {
-
             String token = header.substring(7);
-
             DecodedJWT jwt = JwtUtil.validateToken(token);
-
             String teacherId = jwt.getSubject();
-
             System.out.println("Authenticated user: " + teacherId);
         }
 
