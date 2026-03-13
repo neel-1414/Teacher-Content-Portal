61b8e6041ec81cc317647b23896ee14ff6aa0855
neel-1414 <neelyshinde1@gmail.com>
2026-03-14 01:14:28 +0530

    AuthResponse added

---
+++ src/main/java/com/portal/teachercontentportal/dto/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.portal.teachercontentportal.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AuthResponse {
+    private final String token;
+    public AuthResponse(String token)
+    {
+        this.token = token;
+    }
+}
