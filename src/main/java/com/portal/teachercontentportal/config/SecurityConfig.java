924d1cbcd3f53e1980ebbfc1471f91694c2c1c6b
neel-1414 <neelyshinde1@gmail.com>
2026-03-18 00:54:04 +0530

    changes

--- src/main/java/com/portal/teachercontentportal/config/SecurityConfig.java
+++ src/main/java/com/portal/teachercontentportal/config/SecurityConfig.java
@@ -3,7 +3,6 @@ import com.portal.teachercontentportal.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
